8e4e8423f49d9ac0eb41f75a63a1649e420ef5cb
adri-2 <adriensani@gmail.com>
2024-02-29 20:52:40 +0100

    1e commit mangalib

--- mangalib/models.py
+++ mangalib/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Auteur(models.Model):
-    nom = models.CharField(max_length=64, unique = True)
+    nom = models.CharField(max_length=64, unique = True,verbose_name="Nom")
+    class Meta:
+        verbose_name ="Auteur"
+        verbose_name_plural ="Auteurs"
+    def __str__(self):
+        return self.nom
 
 class livre(models.Model):
-    titre = models.CharField(max_length = 32, unique=True)
-    quantity = models.IntegerField(default=1)
-    auteur = models.ForeignKey(Auteur, on_delete=models.DO_NOTHING)
+    titre = models.CharField(max_length = 32, unique=True,verbose_name="Titre")
+    quantity = models.IntegerField(default=1,verbose_name="Quantité")
+    auteur = models.ForeignKey(Auteur, on_delete=models.DO_NOTHING,verbose_name="Auteur")
+    class Meta:
+        verbose_name ="Livre"
+        verbose_name_plural ="Livres"
+    def __str__(self):
+        return self.titre
 
