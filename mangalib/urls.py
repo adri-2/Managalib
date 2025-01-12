3c4c5c2ac1703867d1db01b8246612eb1f11086e
adri-2 <134091568+adri-2@users.noreply.github.com>
2025-01-12 19:05:57 +0100

    Update urls.py

--- mangalib/urls.py
+++ mangalib/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "mangalib"
 
 urlpatterns = [
-    path('mangalib/',views.index, name='index'),  # manga/
+    path('',views.index, name='index'),  # manga/
     path('<int:livre_id>/',views.show,name= 'show'),   # manga/<id>
     path('ajouter_livre/',views.add_livre,name='add_livre'),
     path('modifie_livre/<int:livre_id>/',views.edit_livre,name='edit_livre'),
